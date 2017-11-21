226c8c1b2e6e410514156b7dabb1172933d2c76b
Radek Zlebcik <zlebcr@mail.desy.de>
2017-11-22 00:50:57 +0100

    Run for multiple aS

--- test/farm/runjobs.sh
+++ test/farm/runjobs.sh
@@ -1,8 +1,15 @@
 address=/afs/desy.de/user/z/zlebcr/h1/TMD/Krakow/bkEvol/test/
 
+outputDir=/nfs/dust/cms/user/zlebcr/Krakow/storage/eq8
+
+for fileFull in $outputDir/*
+do
+    mkdir $fileFull/convMats
+    file=`basename $fileFull`
+
 for i in `seq -w 0 45`
 do
-name=bkEvol_${i}
+name=bkEvol_${file}_${i}
 #name=disc_${i}
 qsub_file=$address/farm/sub/${name}.sub
 
@@ -13,9 +20,13 @@ echo "#$ -o $address/farm/out/${name}.out" >> $qsub_file
 echo "#$ -e $address/farm/err/${name}.err" >> $qsub_file
 
 echo "cd \$TMPDIR" >> $qsub_file
-echo "time $address/convol $i" >> $qsub_file
+echo "time $address/convol $i < $fileFull/config_${file}.ini " >> $qsub_file
 #echo "$address/disc/a.out" >> $qsub_file
 
-echo "cp conv_??_*.h5  /nfs/dust/cms/user/zlebcr/Krakow/convMat/"  >> $qsub_file
+
+#echo "cp conv_??_*.h5   /nfs/dust/cms/user/zlebcr/Krakow/convMat/"  >> $qsub_file
+echo "cp conv_??_*.h5   $fileFull/convMats"  >> $qsub_file
+
+done
 
 done
