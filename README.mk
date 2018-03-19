1e000e49fa3e28d072d96787d0afa7184df1938d
Radek Zlebcik <radek.zlebcik@cern.ch>
2018-03-19 21:01:41 +0100

    Typos correction

--- README.mk
+++ README.mk
@@ -1,39 +1,40 @@
-The program is using several ways to boost the the computation speed.
+The program is using several ways to boost the computation speed.
 
-When fitting the HERA data there are 3 elementary steps which can be cumputionally intence:
+When fitting the HERA data there are 3 elementary steps which can be computationally intense:
 1) Calculation of the evolution kernel (initialPDF -> PDF)
 2) Calculation of the convolution kernel (PDF -> F2, FL, sigmaR)
-3) Applying convolution and evolution kernel (called several hundered of times when fitting)
+3) Applying convolution and evolution kernel (called several hundred of times when fitting)
 
-The paralelisation of the step 1) and 2) is trivial as the kernel cubes (3D matrixes) can be calculated independently for each z (evolution kernel) and each Q2 (convolution kernel).
+The parallelization of the step 1) and 2) is trivial as the kernel cubes (3D matrices) can be calculated independently for each z (evolution kernel) and each Q2 (convolution kernel).
 These kernels can be consequently stored into the files (currently HDF5 is used).
 Fitting then represents only loading the kernels form hdf5 files and applying them multiple times to the inputPDF to find the minimum.
 
-Paralelisation applyed in each step:
+Parallelization applied in each step:
 1) Evolution kernel
-Currently openMP is coded as default to use all CPU corres.
-In addition the openMPI paralelisation is also impremented for systems with shared memory (~100 cores).  In case that only one CPU is availabe this paralelisation has no effect.
-The batch caculation of the evolution kernel can be easily implremented but as there is not large time issue, not used yet.
+Currently openMP is coded as default to use all CPU cores.
+In addition the openMPI parallelization is also implemented for systems with shared memory (~100 cores).  In case that only one CPU is available this parallelization has no effect.
+The batch calculation of the evolution kernel can be easily implemented but as there is not large time issue, not used yet.
 
 2) Convolution kernel
 The calculation requires multidimensional integration which is quite time consuming.
-To boost it, each q2 node is calculated as one job at batch system (~50jobs).
+To boost it, each q2 node is calculated as one job at batch system (~50 jobs).
 Each such job employs openMP to make use of all cores of the machine.
 
 3) Application of the kernels
-This last step is purely about linear algebra but cannot be paralised trivialy.
+This last step is purely about linear algebra but cannot be parallelized trivially.
 The PDF always depends only on PDF at higher x-values.
-Therefore, the next, lower x-value depens on the privous, higher, one.
-To boost such calculation the special library for linear algebra (armadillo using BLAS precedures) is used.
-The BLAS procedures internaly use openMP and SIMD paralelelisations. 
-Futhermore, the evaluation of PDF for single x-value is paralised by openMPI.
+Therefore, the next, lower x-value depends on the previous, higher, one.
+To boost such calculation the special library for linear algebra (armadillo using BLAS procedures) is used.
+The BLAS procedures internally use openMP and SIMD parallelization. 
+Furthermore, the evaluation of discretized "integrals" for single x-value is parallelized by openMPI.
 
 As an alternative, also the GPU-based kernel application is implemented.
-In this case, the BLAS procedures are evaluated at GPU within CUDA.
-It can speed up the calculation substantionaly but nVidia GPU is required.
+In that case, the BLAS procedures are evaluated at GPU within CUDA.
+It can speed up the calculation substantially but nVidia GPU is required.
 
-Instalation
-The code is writen in such a way to be able to be executed on usuall laptop.
--- instalation of ROOT, as Minuit is used for minimalization.
+Installation
+The code is written in such a way to be able to be executed on usual laptop.
+It requires:
+-- ROOT, as Minuit is used for minimization.
 -- HDF5 library for writing kernels to the files
 -- openMPI libraries
